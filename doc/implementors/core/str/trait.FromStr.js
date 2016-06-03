642c33e4df9c68aac184990a791c66bd3c8f512b
Rutger <schoorstra_rutger@hotmail.com>
2016-06-03 21:33:43 +0200

    m3u8 docs

---
+++ doc/implementors/core/str/trait.FromStr.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['m3u8_rs'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html' title='core::str::FromStr'>FromStr</a> for <a class='enum' href='m3u8_rs/playlist/enum.AlternativeMediaType.html' title='m3u8_rs::playlist::AlternativeMediaType'>AlternativeMediaType</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html' title='core::str::FromStr'>FromStr</a> for <a class='enum' href='m3u8_rs/playlist/enum.MediaPlaylistType.html' title='m3u8_rs::playlist::MediaPlaylistType'>MediaPlaylistType</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
